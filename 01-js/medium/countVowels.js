12690b532883f10420c29a934100d6d8d991b58a
Mayank Mehra <mayankmehra13.04@gmail.com>
2024-03-02 15:53:41 +0530

    Count Vowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,13 @@
 */
 
 function countVowels(str) {
-    // Your code here
+    let vowels = 0;
+    for (let i = 0; i < str.length; i++){
+      if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E' || str[i] == 'i' || str[i] == 'I' || str[i] == 'o' || str[i] == 'O' || str[i] == 'u' || str[i] == 'U' ){
+        vowels++;
+      }
+    }
+    return vowels;
 }
 
 module.exports = countVowels;
