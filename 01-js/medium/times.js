2cac844456d73747da1d031d6dc67bd002f0b08c
Mayank Mehra <mayankmehra13.04@gmail.com>
2024-03-03 09:14:16 +0530

    Time

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let initialTime = new Date().getTime();
+    let sum = 0;
+    for (let i = 0; i < n; i++){
+        sum += i;
+    }
+    console.log(sum);
+    let exitTime = new Date().getTime();
+    let totalTimeTaken = exitTime - initialTime;
+    console.log("Total time taken in Milliseconds : " + totalTimeTaken);
 }
+
+calculateTime(1000);
