fe9ed06f5f6c750d8eb04b7f7841e0fe8917c9fd
Mayank Mehra <139584956+mmehraa@users.noreply.github.com>
2024-03-03 09:41:14 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,22 +14,22 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  console.log(transactions);
-  const totals = {};
-  transactions.forEach((transaction) => {
-    const { category, price } = transaction;
-    if (!totals[category]) {
-      totals[category] = price;
-    } else {
-      totals[category] += price;
-    }
-  });
+   console.log(transactions);
+   const totals = {};
+   transactions.forEach((transaction) => {
+     const { category, price } = transaction;
+     if (!totals[category]) {
+       totals[category] = price;
+     } else {
+       totals[category] += price;
+     }
+   });
 
-  const result = [];
-  Object.keys(totals).forEach((category) => {
-    result.push({ category: category, totalSpent: totals[category] });
-  });
-  return result;
+   const result = [];
+   Object.keys(totals).forEach((category) => {
+     result.push({ category: category, totalSpent: totals[category] });
+   });
+   return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
